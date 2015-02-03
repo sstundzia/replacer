4202edf710661219e13da5b07dbd98969bc84894
Simonas Stundzia <simonas.stundzia@adform.com>
2015-02-03 15:32:21 +0200

    Added GUID SmartTag tests with labels and multiple patterns

--- Replacer.Business.Tests/ReplacePatternWrapperTests.cs
+++ Replacer.Business.Tests/ReplacePatternWrapperTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using NUnit.Framework;
 using Replacer.Business.Engine;
@@ -10,47 +11,58 @@ namespace Replacer.Business.Tests
     public class ReplacePatternWrapperTests
     {
         [Test]
-        [TestCase(@"${<NewGUID>}", SmartTagType.PerMatch)]
-        [TestCase(@"${<NewFileGUID>}", SmartTagType.PerFile)]
-        [TestCase(@"${<NewConstGUID>}", SmartTagType.PerBatch)]
-        [TestCase(@"${<NewGUID()>}", SmartTagType.PerMatch)]
-        [TestCase(@"${<NewFileGUID()>}", SmartTagType.PerFile)]
-        [TestCase(@"${<NewConstGUID()>}", SmartTagType.PerBatch)]
-        [TestCase(@"${<NewGUID(A)>}", SmartTagType.PerMatch)]
-        [TestCase(@"${<NewFileGUID(A)>}", SmartTagType.PerFile)]
-        [TestCase(@"${<NewConstGUID(A)>}", SmartTagType.PerBatch)]
-        public void Test_GUID_SmartTag(string replacePattern, SmartTagType type)
+        [TestCase(new []{@"${<NewGUID>}"}, SmartTagType.PerMatch)]
+        [TestCase(new []{@"${<NewFileGUID>}"}, SmartTagType.PerFile)]
+        [TestCase(new []{@"${<NewConstGUID>}"}, SmartTagType.PerBatch)]
+        [TestCase(new []{@"${<NewGUID()>}"}, SmartTagType.PerMatch)]
+        [TestCase(new []{@"${<NewFileGUID()>}"}, SmartTagType.PerFile)]
+        [TestCase(new []{@"${<NewConstGUID()>}"}, SmartTagType.PerBatch)]
+        [TestCase(new []{@"${<NewGUID(A)>}"}, SmartTagType.PerMatch)]
+        [TestCase(new []{@"${<NewFileGUID(A)>}"}, SmartTagType.PerFile)]
+        [TestCase(new []{@"${<NewConstGUID(A)>}"}, SmartTagType.PerBatch)]
+        [TestCase(new[] { @"${<NewGUID(A)>}", @"${<NewGUID(B)>}" }, SmartTagType.PerMatch)]
+        [TestCase(new[] { @"${<NewFileGUID(A)>}", @"${<NewFileGUID(B)>}" }, SmartTagType.PerFile)]
+        [TestCase(new[] { @"${<NewConstGUID(A)>}", @"${<NewConstGUID(B)>}" }, SmartTagType.PerBatch)]
+        public void Test_GUID_SmartTag(string[] replacePatterns, SmartTagType type)
         {
-            ReplacePattern pattern = new ReplacePattern
-            {
-                Find = @"Test",
-                Replace = replacePattern,
-                ReplaceType = ReplaceType.Regex
-            };
-            ReplacePatternWrapper wrapper = new ReplacePatternWrapper(pattern);
+            List<ReplacePatternWrapper> wrappers =
+                replacePatterns.Select((pattern, i) =>
+                    new ReplacePatternWrapper(new ReplacePattern
+                    {
+                        Find = @"Test" + (i + 1).ToString(CultureInfo.InvariantCulture),
+                        Replace = pattern,
+                        ReplaceType = ReplaceType.Regex
+                    }))
+                    .ToList();
 
-            List<string> input = Enumerable.Repeat(@"Test", 3).ToList();
+
+            List<string> input =
+                replacePatterns.SelectMany((pattern, i) =>
+                    Enumerable.Repeat(
+                        @"Test" + (i + 1).ToString(CultureInfo.InvariantCulture),
+                        3))
+                    .ToList();
 
             // First Batch:
             AllAvailableSmartTags.Reset();
 
-            List<string> part1 = input.Select(wrapper.ProcessPattern).ToList();
+            List<Tuple<string, string>> part1 = input.Select(s => new Tuple<string, string>(s, wrappers.Aggregate(s, (sourceString, wrapper) => wrapper.ProcessPattern(sourceString)))).ToList();
 
             AllAvailableSmartTags.ResetFileTags();
 
-            List<string> part2 = input.Select(wrapper.ProcessPattern).ToList();
+            List<Tuple<string, string>> part2 = input.Select(s => new Tuple<string, string>(s, wrappers.Aggregate(s, (sourceString, wrapper) => wrapper.ProcessPattern(sourceString)))).ToList();
 
             // Second Batch:
             AllAvailableSmartTags.Reset();
 
-            List<string> part3 = input.Select(wrapper.ProcessPattern).ToList();
+            List<Tuple<string, string>> part3 = input.Select(s => new Tuple<string, string>(s, wrappers.Aggregate(s, (sourceString, wrapper) => wrapper.ProcessPattern(sourceString)))).ToList();
 
             AllAvailableSmartTags.ResetFileTags();
 
-            List<string> part4 = input.Select(wrapper.ProcessPattern).ToList();
+            List<Tuple<string, string>> part4 = input.Select(s => new Tuple<string, string>(s, wrappers.Aggregate(s, (sourceString, wrapper) => wrapper.ProcessPattern(sourceString)))).ToList();
 
             // Validate:
-            IEnumerable<string> resultsForValidation;
+            IEnumerable<Tuple<string, string>> resultsForValidation;
             if (type == SmartTagType.PerMatch)
             {
                 // All results have to be unique:
@@ -59,34 +71,39 @@ namespace Replacer.Business.Tests
             else if (type == SmartTagType.PerFile)
             {
                 // All results for single file have to match. Separate files must differ:
-                Assert.That(part1.Distinct().Count(), Is.EqualTo(1));
-                Assert.That(part2.Distinct().Count(), Is.EqualTo(1));
-                Assert.That(part3.Distinct().Count(), Is.EqualTo(1));
-                Assert.That(part4.Distinct().Count(), Is.EqualTo(1));
-                resultsForValidation = new[]
-                {
-                    part1[0],
-                    part2[0],
-                    part3[0],
-                    part4[0]
-                };
+                AssertDistinctCountEqualsOne(part1);
+                AssertDistinctCountEqualsOne(part2);
+                AssertDistinctCountEqualsOne(part3);
+                AssertDistinctCountEqualsOne(part4);
+
+                resultsForValidation =
+                    part1.Distinct()
+                        .Union(part2.Distinct())
+                        .Union(part3.Distinct())
+                        .Union(part4.Distinct());
             }
             else
             {
-                Assert.That(part1.Union(part2).Distinct().Count(), Is.EqualTo(1));
-                Assert.That(part3.Union(part4).Distinct().Count(), Is.EqualTo(1));
-                resultsForValidation = new[]
-                {
-                    part1[0],
-                    part3[0]
-                };
+                AssertDistinctCountEqualsOne(part1.Union(part2));
+                AssertDistinctCountEqualsOne(part3.Union(part4));
+                resultsForValidation =
+                    part1.Union(part2).Distinct()
+                        .Union(part3.Union(part4).Distinct());
             }
 
             Assert.That(resultsForValidation, Is.EquivalentTo(resultsForValidation.Distinct()));
 
             // Validate result as GUID:
             Guid guid;
-            Assert.That(resultsForValidation.Select(g => Guid.TryParseExact(g, "D", out guid)).Distinct().Single(), Is.EqualTo(true));
+            Assert.That(resultsForValidation.Select(g => Guid.TryParseExact(g.Item2, "D", out guid)).Distinct().Single(), Is.EqualTo(true));
+        }
+
+        private static void AssertDistinctCountEqualsOne(IEnumerable<Tuple<string, string>> input)
+        {
+            Assert.That(
+                    input.GroupBy(r => r.Item1, r => r.Item2)
+                    .Select(g => g.Distinct().Count())
+                    .Distinct().Count(), Is.EqualTo(1));
         }
     }
 }
